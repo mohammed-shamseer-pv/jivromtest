4116890aae653d9f99f0f967228d9406e07d6679
shamseer <mohammedshamseerp@gmail.com>
2022-05-06 12:50:54 -0700

    chaing

---
+++ assets/js/clint.js
@@ -0,0 +1,30 @@
+$(function() {
+    var $clientslider = $('#clientlogo');
+    var clients = $clientslider.children().length;
+    var clientwidth = (clients * 220); 
+    $clientslider.css('width', clientwidth);
+    var rotating = true;
+    var clientspeed = 1800;
+    var seeclients = setInterval(rotateClients, clientspeed);
+    $(document).on({
+      mouseenter: function() {
+        rotating = false;
+      },
+      mouseleave: function() {
+        rotating = true;
+      }
+    }, '#ourclients');
+    function rotateClients() {
+      if (rotating != false) {
+        var $first = $('#clientlogo li:first');
+        $first.animate({
+          'margin-left': '-220px'
+        }, 2000, function() {
+          $first.remove().css({
+            'margin-left': '0px'
+          });
+          $('#clientlogo li:last').after($first);
+        });
+      }
+    }
+  });
